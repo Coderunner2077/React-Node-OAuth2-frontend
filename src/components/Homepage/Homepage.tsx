b0416f139b308089c3b926a6f8fe9da0e0f73809
TyranReacts <s.mossa1990@gmail.com>
2021-08-06 10:45:01 +0200

    Removed useless files of the project, added react-router-dom

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function Homepage(props: Props) {
+    const {} = props
+
+    return (
+        <div>
+            <h1>Welcome To My Website</h1>
+        </div>
+    )
+}
+
+export default Homepage
---
+++ src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function LoginPage(props: Props) {
+    const {} = props
+
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
+
+export default LoginPage;

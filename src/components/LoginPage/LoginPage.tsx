2ce1dc8a07524a44d9abba111c36e3ba1755e747
TyranReacts <s.mossa1990@gmail.com>
2021-08-07 12:29:40 +0200

    Added Facebook login functionality

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import googleImage from "../../assets/googleImage.png";
+import facebookImage from "../../assets/facebookImage.png";
 import styles from "./LoginPage.module.css";
 
 function LoginPage() {
-    const login = () => {
+    const loginGoogle = () => {
         window.open("http://localhost:4000/auth/google", "_self");
+    }
+
+    const loginFacebook = () => {
+        window.open("http://localhost:4000/auth/facebook", "_self");
     }
 
     return (
         <div className={styles.loginPage}>
             <div className={styles.loginForm}>
                 <h1>Login</h1>
-                <div className={styles.googleContainer} onClick={login}>
+                <div className={styles.googleContainer} onClick={loginGoogle}>
                     <img src={googleImage} alt="Google Icon" />
                     <p>Login With Google</p>
+                </div>
+                <div className={styles.facebookContainer} onClick={loginFacebook}>
+                    <img src={facebookImage} alt="Google Icon" />
+                    <p>Login With Facebook</p>
                 </div>
             </div>
         </div>
